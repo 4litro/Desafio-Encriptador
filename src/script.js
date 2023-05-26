3cb510f0e377c801920b421616648feac64ad921
4litro <a219201031@unison.mx>
2023-05-25 21:45:37 -0700

    añadiendo variables que luego usare y la queso

---
+++ src/script.js
@@ -0,0 +1,21 @@
+
+//Variables extraidas del HTML
+//seccion izquierda y sus variables
+const txtBox = document.getElementsByClassName("txt-box");
+const btnEncriptar = document.getElementsByClassName("btn-encriptar");
+const btnDesencriptar = document.getElementsByClassName("btn-desencriptar");
+const btnLimpiar = document.getElementsByClassName("btn-limpiar");
+
+//seccion derecha y sus variables
+const campoResultado = document.getElementsByClassName("campo-resultado");
+const contParrafoResultado = document.getElementsByClassName("container-parrafo-resultado");
+const txtResultado = document.getElementsByClassName("txt-resultado");
+const contResultado = document.getElementsByClassName("container-parrafo-resultado");
+const contDibujo = document.getElementsByClassName("container-imagen-dibujo");
+const msjNoEncontrado = document.getElementsByClassName("msj-no-encontrado");
+const contCopiar = document.getElementsByClassName("container-copiar");
+const btnCopiar = document.getElementsByClassName("btn-copiar");
+
+ function encrypt(){
+    //declaramos la viarable
+ }
